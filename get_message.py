da1bffcd46038023c557a49205caf2bd15fdc906
alex <alexander.mellor@ap.mell.net>
2025-04-01 18:58:41 +0100

    suggest some small changes

--- get_message.py
+++ get_message.py
@@ -1,26 +1,34 @@
 import pandas as pd
 
-def extract_table(url):
-    table_list = pd.read_html(url)
-    table = table_list[0]
-    table.columns = ["x", "character", "y"]
-    table = table.iloc[1:].reset_index(drop=True)
-    table["x"] = table["x"].astype(int)
-    table["y"] = table["y"].astype(int)
-    table["character"] = table["character"].apply(lambda c: c.encode("latin1").decode("utf-8"))
-    return table
+URL = "https://docs.google.com/document/d/e/2PACX-1vSZ1vDD85PCR1d5QC2XwbXClC1Kuh3a4u0y3VbTvTFQI53erafhUkGot24ulET8ZRqFSzYoi3pLTGwM/pub"
 
-def grid_map(table):
+
+def fetch_grid_dataframe(grid_url):
+    grid_list = pd.read_html(grid_url)
+    grid = grid_list[0]
+    grid.columns = ["x", "character", "y"]
+    grid = grid.iloc[1:].reset_index(drop=True)
+    grid["x"] = grid["x"].astype(int)
+    grid["y"] = grid["y"].astype(int)
+    grid["character"] = grid["character"].apply(
+        lambda character: character.encode("latin1").decode("utf-8")
+    )
+    return grid
+
+
+def format_grid(table):
     max_x = table["x"].max()
     max_y = table["y"].max()
-    grid = [[" " for i in range(max_x + 1)] for i in range(max_y + 1)]
-    for i, row in table.iterrows():
+    grid = [[" " for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+    for _, row in table.iterrows():
         grid[row["y"]][row["x"]] = row["character"]
     return "\n".join("".join(row) for row in reversed(grid))
 
-def print_grid(url):
-    table = extract_table(url)
-    print(grid_map(table))
 
-url = "https://docs.google.com/document/d/e/2PACX-1vSZ1vDD85PCR1d5QC2XwbXClC1Kuh3a4u0y3VbTvTFQI53erafhUkGot24ulET8ZRqFSzYoi3pLTGwM/pub"
-print_grid(url)
+def print_grid(grid_url):
+    grid = fetch_grid_dataframe(grid_url)
+    print(format_grid(grid))
+
+
+if __name__ == "__main__":
+    print_grid(URL)
